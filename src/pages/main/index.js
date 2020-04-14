fd615fd7119359e0453c2a0c55ea6f009fb76cd9
Douglas Rocha <douglascorreia17@gmail.com>
2020-04-14 16:04:35 -0300

    Estruturando melhor o código do state.

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faCheck, faEdit } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import FlipMove from "react-flip-move";
 
@@ -11,23 +10,22 @@ export default class Main extends Component {
 
     this.state = {
       todos: [],
-      currentToDo: {
-        text: "",
-        key: "",
-        complete: false,
-      },
+      inputText: "",
     };
 
     this.handleChanged = this.handleChanged.bind(this);
     this.addToDo = this.addToDo.bind(this);
     this.deleteToDo = this.deleteToDo.bind(this);
+    this.editToDo = this.editToDo.bind(this);
     this.completeToDo = this.completeToDo.bind(this);
   }
 
+  // Toda vez que o DOM for alterado, recuperá o state atual e amarzenar no local storage.
   componentDidUpdate() {
     localStorage.setItem("toDoList", JSON.stringify(this.state.todos));
   }
 
+  // Quando o DOM estiver pronto, irá recuperar o state armazenado no local storage e apresentar na página
   componentDidMount() {
     const todos =
       JSON.parse(localStorage.getItem("toDoList")) || this.state.todos;
@@ -39,49 +37,45 @@ export default class Main extends Component {
 
   handleChanged = (event) => {
     this.setState({
-      currentToDo: {
-        text: event.target.value,
-        key: Date.now(),
-        complete: false,
-      },
+      inputText: event.target.value,
     });
   };
 
   addToDo = (event) => {
     event.preventDefault();
 
-    const {
-      currentToDo: { text },
-    } = this.state;
+    const { inputText } = this.state;
 
-    if (text.length === 0) {
+    if (inputText.length === 0) {
       alert("Campo vazio!");
       return;
     }
 
-    let newToDo = this.state.currentToDo;
+    let newToDo = {
+      text: this.state.inputText,
+      id: Date.now(),
+      complete: false,
+    };
 
     this.setState((prevState) => ({
       todos: [...prevState.todos, newToDo],
-      currentToDo: {
-        text: "",
-        key: "",
-      },
+      inputText: "",
     }));
   };
 
-  deleteToDo = (key) => {
-    // Vai percorrer todo o vetor, a key que for igual, irá deletar
-    const filteredItems = this.state.todos.filter((todo) => todo.key !== key);
+  deleteToDo = (id) => {
+    // Vai percorrer todo o vetor, a id que for igual, irá deletar
+    const filteredItems = this.state.todos.filter((todo) => todo.id !== id);
 
     this.setState({
       todos: filteredItems,
     });
   };
 
-  completeToDo = (key) => {
+  completeToDo = (id) => {
+    // Vai percorrer todo o vetor, a id que for igual, irá alterar a key complete para true
     const updateToDos = this.state.todos.filter((todo) => {
-      if (todo.key === key) {
+      if (todo.id === id) {
         todo.complete = !todo.complete;
       }
 
@@ -90,6 +84,19 @@ export default class Main extends Component {
 
     this.setState({
       todos: updateToDos,
+    });
+  };
+
+  editToDo = (id) => {
+    // Vai percorrer todo o vetor, a id que for igual, irá editar
+    const editToDo = this.state.todos.filter((todo) => {
+      if (todo.id === id) {
+        this.setState({
+          inputText: todo.text,
+        });
+      }
+
+      return todo;
     });
   };
 
@@ -104,36 +111,45 @@ export default class Main extends Component {
           <form onSubmit={this.addToDo}>
             <input
               type="text"
-              value={this.state.currentToDo.text}
+              value={this.state.inputText}
               onChange={this.handleChanged}
             />
 
             <input type="submit" value="+" />
+            <button onClick={this.editToDo}>
+              <FontAwesomeIcon className="faicons" icon={faEdit} />
+            </button>
           </form>
         </div>
 
         <div className="lists_activities">
           <FlipMove duration={250} easing="ease-out" style={fullWidth}>
             {todos.map((todo) => (
-              <div key={todo.key} className="activities__card">
+              <div key={todo.id} className="activities__card">
                 <div className="activities__card__info">
                   <div className="activities__card__info--title">
                     <h2>{todo.text}</h2>
                   </div>
                   <div className="activities__card__info__button">
                     <div
-                      className={`card__info__button--done ${
+                      className={`card__info__button--complete ${
                         todo.complete && "complete"
                       }`}
                     >
-                      <button onClick={() => this.completeToDo(todo.key)}>
+                      <button onClick={() => this.completeToDo(todo.id)}>
                         <FontAwesomeIcon className="faicons" icon={faCheck} />
                       </button>
                     </div>
 
                     <div className="card__info__button--delete">
-                      <button onClick={() => this.deleteToDo(todo.key)}>
+                      <button onClick={() => this.deleteToDo(todo.id)}>
                         <FontAwesomeIcon className="faicons" icon={faTrash} />
+                      </button>
+                    </div>
+
+                    <div className="card__info__button--edit">
+                      <button onClick={() => this.editToDo(todo.id)}>
+                        <FontAwesomeIcon className="faicons" icon={faEdit} />
                       </button>
                     </div>
                   </div>
